780332f0ac4638c831bb4d2095af3e31bf7424db
Sers Théophile <theophile.sers@yahoo.com>
2024-05-22 13:37:53 +0200

    Add Category section

---
+++ WaveLord/src/views/admin/Category.jsx
@@ -0,0 +1,112 @@
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+import Pagination from '../Pagination';
+import '../../scss/Pagination.scss'
+import '../../scss/Category.scss'
+import {FaEdit, FaImage, FaTrash} from 'react-icons/fa'
+
+
+const Category = () => {
+
+    const [currentPage, setCurrentPage] = useState(1);
+    const [searchValue, setSearchValue] = useState('');
+    const [parPage, setParPage] = useState(5);
+    const [show, setShow] =  useState(false);
+
+
+    return (
+        <div className='container'>
+            test
+            <div className='second-container'>
+                <div className='gauche-container'>
+                    <div className="gauche">
+                        <select onChange={(e) => setParPage(parseInt(e.target.value))}>
+                                <option value="5">5</option>
+                                <option value="10">10</option>
+                                <option value="20">20</option> 
+                            </select>
+                        <input type="text" placeholder='search'/>
+                    </div>
+
+                        <div className='table-container'>
+                    <table>
+                        <thead>
+                        <tr>
+                            <th>N°</th>
+                            <th>Image</th>
+                            <th>Name</th>
+                            <th>Action</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        {
+                            [1,2,3,4,5].map((d, i) => (
+                            <tr key={i}>
+                                <td scope='row'>#{d}</td>
+                                <td scope='row'>
+                                    <img src="http://localhost:3000/src/assets/admin.jpeg" alt="" />
+                                    
+                                </td>
+                    
+                                <td>{d.payment_status}Tshirt</td>
+                                
+                                <td>
+                                    <div className='actions-container'>
+                                        <Link> <FaEdit className='fa-action'/> </Link>
+                                        <Link> <FaTrash className='fa-action'/> </Link>
+                                    </div>
+                                </td>
+                            </tr>
+                            
+                            ))
+                        }
+                        </tbody>
+                    </table>
+                        </div>
+
+                        <Pagination
+                            pageNumber= {currentPage}
+                            setPageNumber= {setCurrentPage}
+                            totalItem= {50}
+                            parPage= {parPage}
+                            showItem= {3}
+                        />
+                </div>
+                
+
+
+
+
+
+                <div className='droite'>
+                    <h1>Add Category</h1>
+                    <form action="">
+                        <div className='form-container'>
+                            <label htmlFor="name">Category Name</label>
+                            <input type="text" id='name' name='category_name'
+                            placeholder='Category Name' />
+                        </div>
+
+                        <div>
+                            <label className='image-container' htmlFor='image'>
+                                <span><FaImage/></span>
+                                <span>Select Image</span>
+                            </label>
+                            <input type="file" name="file" id="image" />
+
+                            <div>
+                                <button>Add</button>
+                            </div>
+
+
+
+                        </div>
+                    </form>
+                </div>
+            </div>
+            
+        </div>
+    );
+};
+
+export default Category;
