c0377250857dd3b21472da0c2eca313f307e1ef0
Sers Théophile <theophile.sers@yahoo.com>
2024-05-22 13:44:31 +0200

    Add route for Category

--- WaveLord/src/router/routes/adminRoutes.jsx
+++ WaveLord/src/router/routes/adminRoutes.jsx
@@ -2,6 +2,7 @@
 import { lazy } from "react";
 const AdminDashboard = lazy(() => import('../../views/admin/AdminDashboard'));
 const Orders = lazy(() => import('../../views/admin/Orders'));
+const Category = lazy(() => import('../../views/admin/Category'));
 
 export const adminRoutes = [
     {
@@ -14,5 +15,10 @@ export const adminRoutes = [
         element: <Orders />,
         role : 'admin'
     },
-
+    {
+        path: '/admin/dashboard/category',
+        element: <Category />,
+        role : 'admin'
+    },
+    
 ];
