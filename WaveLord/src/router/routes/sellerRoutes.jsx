2266decc8ad5e0fb1a55d4fbd96b61b872066843
Sers Théophile <theophile.sers@yahoo.com>
2024-06-12 13:02:20 +0200

    Add files via upload

--- WaveLord/src/router/routes/sellerRoutes.jsx
+++ WaveLord/src/router/routes/sellerRoutes.jsx
@@ -1,7 +1,9 @@
 import { lazy } from "react";
+
 const Home = lazy(() => import('../../views/Home'));
 const SellerDashboard = lazy(() => import('../../views/seller/SellerDashboard'));
 const AddProduct = lazy(() => import('../../views/seller/AddProduct'));
+const Products = lazy(() => import('../../views/seller/Products'));
 
 export const sellerRoutes = [
     {
@@ -18,6 +20,11 @@ export const sellerRoutes = [
     {
         path: '/seller/dashboard/add-product',
         element: <AddProduct />,
+        ability : ['seller']
+    },
+    {
+        path: '/seller/dashboard/products',
+        element: <Products />,
         ability : ['seller']
     },
 
