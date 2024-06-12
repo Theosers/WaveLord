6c0eac792af1f86f8df8a09ecd90e18976b100c3
Sers Théophile <theophile.sers@yahoo.com>
2024-06-12 15:14:12 +0200

    Adding Payments Page

--- WaveLord/src/router/routes/sellerRoutes.jsx
+++ WaveLord/src/router/routes/sellerRoutes.jsx
@@ -5,6 +5,8 @@ const SellerDashboard = lazy(() => import('../../views/seller/SellerDashboard'))
 const AddProduct = lazy(() => import('../../views/seller/AddProduct'));
 const Products = lazy(() => import('../../views/seller/Products'));
 const DiscountProducts = lazy(() => import('../../views/seller/DiscountProducts')); 
+const Orders = lazy(() => import('../../views/seller/Orders')); 
+const Payments = lazy(() => import('../../views/seller/Payments')); 
 
 export const sellerRoutes = [
     {
@@ -31,6 +33,16 @@ export const sellerRoutes = [
     {
         path: '/seller/dashboard/discount-product',
         element: <DiscountProducts />,
+        ability : ['seller']
+    },
+    {
+        path: '/seller/dashboard/orders',
+        element: <Orders />,
+        ability : ['seller']
+    },
+    {
+        path: '/seller/dashboard/payments',
+        element: <Payments />,
         ability : ['seller']
     },
 
