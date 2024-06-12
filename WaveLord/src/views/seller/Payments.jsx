41e878db35fefe920330263b865411726c88cbe2
Sers Théophile <theophile.sers@yahoo.com>
2024-06-12 15:14:49 +0200

    Adding Payments Page

---
+++ WaveLord/src/views/seller/Payments.jsx
@@ -0,0 +1,139 @@
+import React, { forwardRef } from "react";
+import { MdCurrencyExchange,MdProductionQuantityLimits } from "react-icons/md";
+import { FaUsers } from "react-icons/fa";
+import { FaCartShopping } from "react-icons/fa6"; 
+import Chart from 'react-apexcharts'
+import { Link } from 'react-router-dom';
+import '../../scss/seller/Payments.scss'
+
+import { FixedSizeList as List} from "react-window";
+import '../../scss/admin/PaymentRequest.scss'
+
+function handleOnWheel({ deltaY }) {
+    console.log('handleOnWheel', deltaY)
+}
+
+const outerElementType = forwardRef((props, ref) => (
+    <div ref={ref} onWheel={handleOnWheel} {...props} />
+))
+
+const Payments = () => {
+
+
+    const Row = ({ index, style }) => {
+        
+        return (
+            <div style={style} className="row-container">
+
+                <div>{index + 1}</div>
+                <div>$6528</div>
+                <div>
+                    <span>Pending</span>
+                </div>
+                <div>25 Dec 2023</div>
+                
+                
+
+            </div>
+        )
+    }
+
+
+
+
+
+    return (
+        <div className='admin-dashboard'>
+            <div className='stats'>
+            <div className='stat-card quatre'>
+            <MdCurrencyExchange className='icon' />
+            <div>
+                <h3>$9483</h3>
+                <p>Total Sales</p>
+            </div>
+            </div>
+            <div className='stat-card deux'>
+            <MdCurrencyExchange className='icon' />
+            <div>
+                <h3>$350</h3>
+                <p>Available Amount</p>
+            </div>
+            </div>
+            <div className='stat-card un'>
+            <MdCurrencyExchange className='icon' />
+            <div>
+                <h3>$100</h3>
+                <p>Withdrawal Amount</p>
+            </div>
+            </div>
+            <div className='stat-card trois'>
+            <MdCurrencyExchange className='icon' />
+            <div>
+                <h3>$0</h3>
+                <p>Pending Amount</p>
+            </div>
+            </div>
+            </div>
+
+            <div className="main-content">
+                <div className="SendPendingRequest">
+                    <h3>Send Request</h3>
+                    <form action="">
+                        <input min="0" type="number" name="amount" />
+                        <button>Submit</button>
+                    </form>
+                    <h3>Pending Request</h3>
+                    <div>
+                        <div className="title">
+                            <div>N°</div>
+                            <div>Amount</div>
+                            <div>Status</div>
+                            <div>Date</div>
+                            
+                        </div>
+
+                        {
+                            <List
+                            style={{ minWidth : '340px'}}
+                            className='List'
+                            height={350}
+                            itemCount={9}
+                            itemSize={35}
+                            outerElementType= {outerElementType}
+                            >{Row}</List>
+                        }
+
+                    </div>
+
+                </div>
+                <div className="SuccessWithdraw">
+                    <h3>Sucess Withdraw</h3>
+                    <div>
+                        <div className="title">
+                            <div>N°</div>
+                            <div>Amount</div>
+                            <div>Status</div>
+                            <div>Date</div>
+                            
+                        </div>
+
+                        {
+                            <List
+                            style={{ minWidth : '340px'}}
+                            className='List'
+                            height={350}
+                            itemCount={9}
+                            itemSize={35}
+                            outerElementType= {outerElementType}
+                            >{Row}</List>
+                        }
+
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    )
+};
+
+export default Payments
