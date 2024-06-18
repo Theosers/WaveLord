8e084ec1332c26056e28e6ddae96c21e96487461
Sers Théophile <theophile.sers@yahoo.com>
2024-06-18 08:46:29 +0200

    ProtectRoutes full updated

--- WaveLord/src/router/routes/ProtectRoute.jsx
+++ WaveLord/src/router/routes/ProtectRoute.jsx
@@ -1,32 +1,57 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import { useSelector } from 'react-redux';
 import { Navigate } from 'react-router-dom';
-import { Suspense } from 'react';
 
+const ProtectRoute = ({route,children}) => {
+    const {role, userInfo} = useSelector(state => state.auth)
 
-const ProtectRoute = ({ route, children }) => {
-
-    
-    const { role, userInfo } = useSelector(state => state.auth);
-    console.log(role, userInfo)
-
-    if(role) {
-        console.log('il y a bien un role', role)
-        if (userInfo) {
-            console.log(('il y a bien un userInfo', userInfo))
-            if (userInfo.role === route.role) {
-                return <Suspense fallback={null}>{children}</Suspense>
-            }
-            else {
-                console.log(user.role, route.role)
+    if (role) {
+        if (route.role) {
+            if (userInfo) {
+                if (userInfo.role === route.role) {
+                    if (route.status) {
+                        if (route.status === userInfo.status) {
+                            return <Suspense fallback={null} >{children}</Suspense>
+                        }else {
+                            if (userInfo.status === 'pending') {
+                                return <Navigate to='/seller/account-pending' replace />
+                            } else {
+                                return <Navigate to='/seller/account-deactive' replace />
+                            } 
+                    }
+                
+                }  else {
+                    if (route.visibility) {
+                        if (route.visibility.some(r => r === userInfo.status)) {
+                            return <Suspense fallback={null} >{children}</Suspense>
+                        } else {
+                            return <Navigate to='/seller/account-pending' replace />
+                        }
+                        
+                    } else {
+                        return <Suspense fallback={null} >{children}</Suspense>
+                    }
+                   
+                }
+                
+               }else{
                 return <Navigate to='/unauthorized' replace />
-            }
-        }
-        
-    } else {
-        console.log('final else, pas de role')
+               }
+            } 
+
+
+            
+        } else {
+            if (route.ability === 'seller') {
+                return <Suspense fallback={null} >{children}</Suspense>
+            } 
+        } 
+    }else {
         return <Navigate to='/login' replace />
     }
+ 
+
+    
 };
 
 export default ProtectRoute;
