62d6cfec6e0c4d6df2de80e0f595aa6f9255a45a
Sers Théophile <theophile.sers@yahoo.com>
2024-06-18 09:37:05 +0200

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -15,6 +15,7 @@ app.use(cookieParser());
 dbConnect()
 
 app.use('/api', require('./routes/authRoutes'));
+app.use('/api', require('./routes/dashboard/categoryRoutes'));
 
 app.get('/', (req, res) => {
     res.send('Hello World from my server!');
