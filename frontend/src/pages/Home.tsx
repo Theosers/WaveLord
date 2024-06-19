39bd67f584fc547e175f302f3ad0f87b2eb2ea10
Sers Théophile <theophile.sers@yahoo.com>
2024-06-19 14:48:17 +0200

    Updating Home

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,9 +1,38 @@
 import React from 'react';
 import Header from '../components/Header';
+import Banner from '../components/Banner';
+import Categorys from '../components/Categorys';
+import FeatureProducts from '../components/products/FeatureProducts';
+import Products from '../components/products/Products';
+
+import '../../src/scss/pages/Home.scss'
+
 const Home = () => {
     return (
         <div>
             <Header/>
+            <Banner/>
+            <Categorys/>
+            <FeatureProducts/>
+
+            <div>
+                <div>
+                    <div className='multiple-products-container'>
+                        <div>
+                            <Products title='Latest Product'/>
+                        </div>
+
+                        <div>
+                            <Products title='Top Rated Product'/>
+                        </div>
+
+                        <div>
+                            <Products title='Discount Product'/>
+                        </div>
+
+                    </div> 
+                </div> 
+            </div>
         </div>
     )
 }
