0798017ac56a23e6124fec972101888d921504f5
Sers Théophile <theophile.sers@yahoo.com>
2024-06-20 14:36:21 +0200

    Update Chat.tsx adding a friend SOCKET IO

--- frontend/src/components/dashboard/Chat.tsx
+++ frontend/src/components/dashboard/Chat.tsx
@@ -3,20 +3,30 @@ import { AiOutlineMessage, AiOutlinePlus } from 'react-icons/ai'
 import { GrEmoji } from 'react-icons/gr'
 import { IoSend } from 'react-icons/io5'
 import { Link } from 'react-router-dom'
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { Link, useParams } from 'react-router-dom'
 
 import io from 'socket.io-client'
+import { add_friend } from '../../store/reducers/chatReducer';
 const socket = io('http://localhost:5000')
+
 
 const Chat = () => {
 
+    const dispatch = useDispatch()
     const {sellerId} = useParams()
     const {userInfo } = useSelector(state => state.auth)
 
     useEffect(() => {
         socket.emit('add_user',userInfo.id, userInfo)
     },[])
+
+    useEffect(() => {
+        dispatch(add_friend({
+            sellerId: sellerId || "",
+            userId: userInfo.id
+        }))
+    },[sellerId])
     
     return (
         <div>
