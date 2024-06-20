7c92f7bcc880877171376bc08bb8d3230bf2324f
Sers Théophile <theophile.sers@yahoo.com>
2024-06-20 09:32:46 +0200

    Update Payment.tsx

--- frontend/src/pages/Payment.tsx
+++ frontend/src/pages/Payment.tsx
@@ -1,9 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Header from './../components/Header';
+import Footer from './../components/Footer';
+import { useLocation } from 'react-router-dom';
 
 const Payment = () => {
+
+    const { state: {price,items,orderId}} = useLocation()
+    const [paymentMethod, setPaymentMethod] = useState('stripe')
+    
     return (
         <div>
-            Payment Page 
+           <Header/>
+           <section>
+            <div>
+                <div>
+                    <div>
+                        <img src="http://localhost:3000/public/images/payment/stripe.png" alt="" />
+                    </div>
+                    <span>Stripe</span>
+                </div>                  
+            </div>
+        </section>
+        <Footer/>
         </div>
     );
 };
