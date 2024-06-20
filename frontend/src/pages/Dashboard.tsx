6c9f5c7ce5de2444e3b11e639a707edeb32be611
Sers Théophile <theophile.sers@yahoo.com>
2024-06-20 09:56:23 +0200

    Update Dashboard.tsx

--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -1,19 +1,63 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 import { FaList } from 'react-icons/fa';
+import { Link, Outlet } from 'react-router-dom';
+import { IoIosHome } from "react-icons/io";
+import { FaBorderAll } from "react-icons/fa6";
+import { FaHeart } from "react-icons/fa";
+import { IoChatbubbleEllipsesSharp } from "react-icons/io5";
+import { IoMdLogOut } from "react-icons/io";
+import { RiLockPasswordLine } from "react-icons/ri";
 
 const Dashboard = () => {
+
+    const [filterShow, setFilterShow] = useState(false)
+    
     return (
         <div>
            <Header/>
            <div>
                 <div>
-                    <div>
-                        <button><FaList/> </button>
-                    </div>
+                    <button><FaList/> </button>
                 </div>
            </div>
+
+
+
+            <div>
+                    <ul> 
+                        <li>
+                            <span><IoIosHome /></span>
+                            <Link to='/dashboard'>Dashboard </Link>
+                        </li>
+                        <li>
+                            <span><FaBorderAll/></span>
+                            <Link to='/dashboard'>My Orders </Link>
+                        </li>
+                        <li>
+                            <span><FaHeart/></span>
+                            <Link to='/dashboard'>Wishlist </Link>
+                        </li>
+                        <li>
+                            <span><IoChatbubbleEllipsesSharp/></span>
+                            <Link to='/dashboard'>Chat  </Link>
+                        </li>
+                        <li>
+                            <span><RiLockPasswordLine/></span>
+                            <Link to='/dashboard'>Change Password  </Link>
+                        </li>
+                        <li>
+                            <span><IoMdLogOut/></span>
+                            <Link to='/dashboard'>Logout </Link>
+                        </li> 
+                </ul> 
+
+                <div>
+                    <Outlet/>
+                </div>
+            </div>        
+           
            <Footer/>
         </div>
     );
