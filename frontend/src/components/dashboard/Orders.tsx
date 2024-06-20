ebb22faae9f4e4f7d772b50d48ee43a440dd4676
Sers Théophile <theophile.sers@yahoo.com>
2024-06-20 11:09:29 +0200

    Update Orders.tsx with dynamic tbody

--- frontend/src/components/dashboard/Orders.tsx
+++ frontend/src/components/dashboard/Orders.tsx
@@ -1,17 +1,33 @@
 import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { Link, useParams } from 'react-router-dom';
+import { Link, useNavigate, useParams } from 'react-router-dom';
 import { get_orders } from '../../store/reducers/orderReducer';
 
 const Orders = () => {
     const [state, setState] = useState('all')
 
+    const navigate = useNavigate()
     const dispatch = useDispatch()
     const { orderId } = useParams()
     const {userInfo} = useSelector(state => state.auth)
+    const { myOrders } = useSelector(state => state.order)
     useEffect(() => {
         dispatch(get_orders({status:state, customerId:userInfo.id}))
-    },[orderId])
+    },[state])
+
+    const redirect = (ord) => {
+        let items = 0;
+        for (let i = 0; i < ord.length; i++) {
+            items = ord.products[i].quantity + items; 
+        }
+        navigate('/payment',{
+            state: {
+                price: ord.price,
+                items,
+                orderId: ord._id 
+            }
+        }) 
+    }
     
     return (
         <div>
@@ -37,14 +53,17 @@ const Orders = () => {
                         </tr>
                     </thead>
                         <tbody>
-                            <tr>
-                                <td scope='row'>#344</td>
-                                <td scope='row'>$233</td>
-                                <td scope='row'>pending</td>
-                                <td scope='row'>pending</td>
+                            {
+                            myOrders.map((o,i) => <tr>
+                                <td scope='row'>#{o._id}</td>
+                                <td scope='row'>${o.price}</td>
+                                <td scope='row'>{o.payment_status }</td>
+                                <td scope='row'>{o.delivery_status}</td>
                                 <td scope='row'>
-                                    <Link><span>View</span></Link>
-                                    <Link><span>Pay Now</span></Link> 
+                                    <Link to={`/dashboard/order/details/${o._id}`}><span>View</span></Link>
+                                    {
+                                      o.payment_status !== 'paid' && <span onClick={() => redirect(o)}>Pay Now</span> 
+                                    }
                                 </td> 
                             </tr>
                 
@@ -59,6 +78,8 @@ const Orders = () => {
                                     <Link><span>Pay Now</span></Link> 
                                 </td> 
                             </tr>
+                            )
+                            } 
                 
                         </tbody>
                 
