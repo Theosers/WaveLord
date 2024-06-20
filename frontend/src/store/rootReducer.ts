daab40ee817621e2c2d2bd97eeaea12c5c3b349b
Sers Théophile <theophile.sers@yahoo.com>
2024-06-20 10:49:57 +0200

    Update rootReducer.ts with dashboardReducer

--- frontend/src/store/rootReducer.ts
+++ frontend/src/store/rootReducer.ts
@@ -1,12 +1,14 @@
 import { authReducer } from "./reducers/authReducer";
 import homeReducer from "./reducers/homeReducer";
 import cardReducer from "./reducers/cardReducer";
+import dashboardReducer from "./reducers/dashboardReducer";
 import orderReducer from "./reducers/orderReducer";
 
 const rootReducer = {
     home: homeReducer,
     auth: authReducer,
     card: cardReducer,
-    order: orderReducer
+    order: orderReducer,
+    dashboard: dashboardReducer
 }
 export default rootReducer;
