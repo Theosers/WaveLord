b188fcf37576900eb73681e650b514cc7e79164c
Sers Théophile <theophile.sers@yahoo.com>
2024-06-20 11:16:30 +0200

    Update OrderDetails.tsx

--- frontend/src/components/dashboard/OrderDetails.tsx
+++ frontend/src/components/dashboard/OrderDetails.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch,useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
 import { get_order_details } from '../../store/reducers/orderReducer';
 
@@ -8,13 +8,28 @@ const OrderDetails = () => {
     const {orderId} = useParams()
     const dispatch = useDispatch()
 
+    const {userInfo} = useSelector(state => state.auth)
+    const {myOrder} = useSelector(state => state.order)
+    
     useEffect(() => {
         dispatch(get_order_details(orderId))
     },[orderId])
 
     return (
         <div>
-            order details 
+            <h2>#{myOrder._id} , <span>{myOrder.date}</span> </h2>
+    
+            <div>
+                <h2>
+                Deliver To : {myOrder.shippingInfo?.name} </h2>
+                <p>
+                    <span>Home</span>
+                    <span>{myOrder.shippingInfo?.address}{myOrder.shippingInfo?.province}{myOrder.shippingInfo?.city}</span>
+                </p>
+                <p>
+                    Email To {userInfo.email }
+                </p>
+            </div>
         </div>
     );
 };
