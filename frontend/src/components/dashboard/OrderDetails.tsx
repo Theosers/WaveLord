0eb3e00b3901fe0da1c2e308c26559b8a0c33082
Sers Théophile <theophile.sers@yahoo.com>
2024-06-20 11:26:14 +0200

    Update OrderDetails.tsx

--- frontend/src/components/dashboard/OrderDetails.tsx
+++ frontend/src/components/dashboard/OrderDetails.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
 import { useDispatch,useSelector } from 'react-redux';
-import { useParams } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 import { get_order_details } from '../../store/reducers/orderReducer';
 
 const OrderDetails = () => {
@@ -30,6 +30,45 @@ const OrderDetails = () => {
                     Email To {userInfo.email }
                 </p>
             </div>
+
+            <div>
+                <h2>Price : ${myOrder.price} Include Shipping</h2>
+                <p> Payment Status : <span className={`a ${myOrder.payment_status === 'paid' ? 'b' : 'c' }`}> {myOrder.payment_status} </span> </p>
+
+                <p> Order Status : <span className={`a ${myOrder.delivery_status === 'paid' ? 'b' : 'c' }`}> {myOrder.delivery_status} </span> </p> 
+            </div>   
+        </div>
+
+        <div>
+            <h2>Order Products </h2>
+            <div>
+                {
+                    myOrder.products?.map((p,i) => <div key={i}>
+                        <div>
+                            <div>
+                               <img src={p.images[0]} alt="" />
+                               <div>
+                                    <Link> {p.name} </Link>
+                                    <p> <span>Brand : {p.brand}</span> </p>
+                                    <p><span>Quantity : {p.quantity}</span></p>
+                               </div>
+                            </div>
+
+                        <div>
+                                <h2>${p.price - Math.floor((p.price * p.discount) / 100)}</h2>
+                                <p>{p.price}</p>
+                                <p>-{p.discount}%</p>
+                        </div>
+
+
+            </div>
+        </div>
+
+                )
+            }
+        </div>
+
+    </div>
         </div>
     );
 };
