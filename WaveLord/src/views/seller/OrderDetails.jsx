9851c7cc622a8f693bbc4f4ff45414f4ce9a465b
Sers Théophile <theophile.sers@yahoo.com>
2024-06-21 13:45:33 +0200

    Update OrderDetails.jsx toasted

--- WaveLord/src/views/seller/OrderDetails.jsx
+++ WaveLord/src/views/seller/OrderDetails.jsx
@@ -1,30 +1,53 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
-import { get_seller_order } from '../../store/Reducers/OrderReducer';
+import { get_seller_order,messageClear, seller_order_status_update } from '../../store/Reducers/OrderReducer';
+import toast from 'react-hot-toast';
 import '../../scss/admin/OrderDetails.scss';
 
 const OrderDetails = () => {
 
     const { orderId } = useParams() 
     const dispatch = useDispatch() 
+    const [status, setStatus] = useState('')
+    
     const { order,errorMessage,successMessage } = useSelector(state => state.order)
+
+    useEffect(() => {
+        setStatus(order?.delivery_status)
+    },[order])
 
     useEffect(() => {
         dispatch(get_seller_order(orderId))
     },[orderId])
+
+    const status_update = (e) => {
+        dispatch(seller_order_status_update({orderId, info: {status: e.target.value} }))
+        setStatus(e.target.value)
+    }
+
+    useEffect(() => { 
+        if (successMessage) {
+            toast.success(successMessage)
+            dispatch(messageClear())  
+        } 
+        if (errorMessage) {
+            toast.error(errorMessage)
+            dispatch(messageClear())  
+        } 
+    },[successMessage,errorMessage])
     
     return (
         <div className='order-details-container'>
             <div className='order-details-box'>
                 <div className='title-container'>
                     <h2>Order Details</h2>
-                    <select name="" id="">
-                        <option value="">pending</option>
-                        <option value="">processing</option>
-                        <option value="">warehouse</option>
-                        <option value="">placed</option>
-                        <option value="">cancelled</option>
+                    <select onChange={status_update} value={status} name="" id="">
+                        <option value="pending">pending</option>
+                        <option value="processing">processing</option>
+                        <option value="warehouse">warehouse</option>
+                        <option value="placed">placed</option>
+                        <option value="cancelled">cancelled</option>
                     </select>
                 </div>
                 <div className='duo-box'>
