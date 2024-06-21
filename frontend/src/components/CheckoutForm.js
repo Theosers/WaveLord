4a7ee19e52bcf7f96bbc97dccb9b09c05b1a0402
Sers Théophile <theophile.sers@yahoo.com>
2024-06-21 14:22:20 +0200

    Update CheckoutForm.js

--- frontend/src/components/CheckoutForm.js
+++ frontend/src/components/CheckoutForm.js
@@ -1,10 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { PaymentElement,LinkAuthenticationElement,useStripe,useElements } from '@stripe/react-stripe-js' 
 
-const CheckoutForm = () => {
+const CheckoutForm = ({orderId}) => {
+
+    localStorage.setItem('orderId',orderId)
+    const stripe = useStripe()
+    const elements = useElements()
+    const [message, setMessage] = useState(null)
+    const [isLoading, setIsLoading] = useState(false)
+
+    const paymentElementOptions = {
+        loyout: 'tabs'
+    }
+
+    const submit = async (e) => {
+        e.preventDefault()
+    }
+
+
     return (
-        <div>
-            sadf
-        </div>
+        <form onSubmit={submit} id='payment-form'>
+            <LinkAuthenticationElement id='link-authentication-element'/>
+            <PaymentElement id='payment-element' options={paymentElementOptions} />
+
+            <button disabled={isLoading || !stripe || !elements} id='submit'>
+                <span id='button-text'>
+                    {
+                        isLoading ? <div>Loading...</div> : "Pay Now"
+                    }
+                </span> 
+            </button>
+               {message && <div>{message}</div>}
+        </form>
     );
 };
 
