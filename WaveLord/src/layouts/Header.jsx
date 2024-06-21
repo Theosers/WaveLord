a66e072857ff8f0349ff95d1379898ff7416615e
Sers Théophile <theophile.sers@yahoo.com>
2024-06-21 10:16:11 +0200

    Update Header.jsx adding dynamic Header

--- WaveLord/src/layouts/Header.jsx
+++ WaveLord/src/layouts/Header.jsx
@@ -7,6 +7,7 @@ import '../scss/layouts/Header.scss';
 const Header = ({showSidebar, setShowSidebar}) => {
 
   const [isScrolled, setIsScrolled] = useState(false);
+  const {userInfo } = useSelector(state => state.auth)
 
   useEffect(() => {
     const handleScroll = () => {
@@ -37,8 +38,8 @@ const Header = ({showSidebar, setShowSidebar}) => {
         <div className='user-info'>
           <div className='user-details'>
             <div className='user-text'>
-              <h2>userInfos.name</h2>
-              <span>userInfos.role</span>
+              <h2>{ userInfo.name }</h2>
+              <span>{ userInfo.role }</span>
             </div>
             <img className='user-image' src='http://localhost:3000/src/assets/admin.jpeg' alt='' />
           </div>
