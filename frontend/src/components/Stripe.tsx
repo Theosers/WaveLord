1d08db3241792b2a44562084a67cfa3284f2dedc
Sers Théophile <theophile.sers@yahoo.com>
2024-06-21 14:20:01 +0200

    Update Stripe.tsx

--- frontend/src/components/Stripe.tsx
+++ frontend/src/components/Stripe.tsx
@@ -1,11 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { loadStripe } from '@stripe/stripe-js' 
+import { Elements } from '@stripe/react-stripe-js' 
+import axios from 'axios';
+import CheckoutForm from './CheckoutForm';
+const stripePromise = loadStripe('pk_test_51Oml5cGAwoXiNtjJgPPyQngDj9WTjawya4zCsqTn3LPFhl4VvLZZJIh9fW9wqVweFYC5f0YEb9zjUqRpXbkEKT7T00eU1xQvjp')
 
-const Stripe = () => {
+const Stripe = ({ price, orderId }) => {
+    const [clientSecret, setClientSecret] = useState('')
+    const apperance = {
+        theme: 'stripe'
+    }
+    const options = {
+        apperance,
+        clientSecret
+    }
+
     return (
         <div>
-            stipe page 
+            {
+                clientSecret ? (
+                    <Elements options={options} stripe={stripePromise}>
+                        <CheckoutForm />
+                    </Elements>
+                ) : <button>Start Payment</button>
+            }
         </div>
     );
-};
+}; 
 
 export default Stripe;
