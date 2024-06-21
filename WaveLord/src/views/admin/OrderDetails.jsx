bd9893bf99f926fc24d7e9e98eb4830f52689c09
Sers Théophile <theophile.sers@yahoo.com>
2024-06-21 13:16:23 +0200

    Update OrderDetails.jsx Get and Show

--- WaveLord/src/views/admin/OrderDetails.jsx
+++ WaveLord/src/views/admin/OrderDetails.jsx
@@ -1,29 +1,36 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { useParams } from 'react-router-dom';
-import { get_admin_order } from '../../store/Reducers/OrderReducer';
+import { admin_order_status_update, get_admin_order } from '../../store/Reducers/OrderReducer';
 import '../../scss/admin/OrderDetails.scss';
 
 const OrderDetails = () => {
 
-    const { orderId } = useParams() 
     const dispatch = useDispatch() 
+    const [status, setStatus] = useState('')
 
     useEffect(() => {
         dispatch(get_admin_order(orderId))
     },[orderId])
+
+    const { orderId } = useParams() 
+
+    const status_update = (e) => {
+        dispatch(admin_order_status_update({orderId, info: {status: e.target.value} }))
+        setStatus(e.target.value)
+    }
     
     return (
         <div className='order-details-container'>
             <div className='order-details-box'>
                 <div className='title-container'>
                     <h2>Order Details</h2>
-                    <select name="" id="">
-                        <option value="">pending</option>
-                        <option value="">processing</option>
-                        <option value="">warehouse</option>
-                        <option value="">placed</option>
-                        <option value="">cancelled</option>
+                    <select onChange={status_update} value={status} name="" id="">
+                        <option value="pending">pending</option>
+                        <option value="processing">processing</option>
+                        <option value="warehouse">warehouse</option>
+                        <option value="placed">placed</option>
+                        <option value="cancelled">cancelled</option>
                     </select>
                 </div>
                 <div className='duo-box'>
@@ -82,23 +89,30 @@ const OrderDetails = () => {
                         </div>
                     
                     </div>
-                    <div className='right-box'>
-                        <div className='seller-order'>
-                        <h2>Seller 1 Order : </h2>
-                        <span>pending</span>
+                    
+                        
+                            {
+                            order?.suborder?.map((o,i) =>  <div className='right-box'>
+                                <div key={i + 20} className='seller-order'>
+                                <h2>Seller {i + 1} Order : </h2>
+                                <span>{o.delivery_status}</span>
+                                </div>
+                                {
+                                o.products?.map((p,i) => <div className='order-items'>
+                        
+                                    <img className='user-image' src={p.images[0]} alt='' />
+                                    <div>
+                                        <h2>{p.name} </h2>
+                                        <p>
+                                            <span>Brand : </span>
+                                            <span>{p.brand}</span>
+                                            <span>Quantity : {p.quantity} </span>
+                                        </p>
+                                    </div>
+                                </div>)
+                                }
                         </div>
-                        <div className='order-items'>
-                            <img className='user-image' src='http://localhost:3000/src/assets/admin.jpeg' alt='' />
-                            <div>
-                                <h2>Product Name here</h2>
-                                <p>
-                                    <span>Brand : </span>
-                                    <span>Easy</span>
-                                    <span>Quantity : 3</span>
-                                </p>
-                            </div>
-                        </div>
-                    </div>
+                        }
                 </div>
             </div>
         </div>
