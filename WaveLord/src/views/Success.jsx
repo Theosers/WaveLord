7b1725ee061c12a09aa5b7e92e7ec5a6b9212239
Sers Théophile <theophile.sers@yahoo.com>
2024-06-21 14:15:55 +0200

    Update Success.jsx loader

--- WaveLord/src/views/Success.jsx
+++ WaveLord/src/views/Success.jsx
@@ -1,12 +1,18 @@
 import React, { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
-import { active_stripe_connect_account } from '../store/Reducers/sellerReducer';
+import { active_stripe_connect_account, messageClear } from '../store/Reducers/sellerReducer';
+import { FadeLoader } from 'react-spinners';
+import error from '../assets/error.png'
+import success from '../assets/success.png'
 
 const Success = () => {
 
     const navigate = useNavigate()
     const dispatch = useDispatch()
+
+    const {loader,successMessage,errorMessage} = useSelector(state => state.seller)
+    
     const queryParams = new URLSearchParams(window.location.search)
     const activeCode = queryParams.get('activeCode')
 
@@ -14,9 +20,25 @@ const Success = () => {
         dispatch(active_stripe_connect_account(activeCode))
     },[activeCode]) 
 
+    const redirect = () => {
+        dispatch(messageClear())
+        navigate('/')
+    }
+    
+
     return (
         <div>
-            success page 
+            {
+                loader ? <FadeLoader/> : errorMessage ? <>
+                    <img src={error} alt="" />
+                    <button onClick={redirect}>Back to Dashboard</button>
+                </> : successMessage && <>
+                <img src={success} alt="" />
+                    <button onClick={redirect}>Back to Dashboard</button>
+                </>
+            }
+
+
         </div>
     );
 };
