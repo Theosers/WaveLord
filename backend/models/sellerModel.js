b4abbc5a66074855911ff9ba998440c7288795e8
Sers Théophile <theophile.sers@yahoo.com>
2024-06-21 11:12:01 +0200

    Update sellerModel.js adding index

--- backend/models/sellerModel.js
+++ backend/models/sellerModel.js
@@ -1,34 +1,34 @@
-const { Schema, model } = require('mongoose');
+const {Schema, model} = require("mongoose");
 
 const sellerSchema = new Schema({
     name: {
         type: String,
-        required: true
+        required : true
     },
     email: {
         type: String,
-        required: true
-        },
+        required : true
+    },
     password: {
         type: String,
-        required: true,
+        required : true,
         select: false
-        },
+    },     
     role: {
         type: String,
-        default: 'seller'
+        default : 'seller'
     },
     status: {
         type: String,
-        default: 'pending'
+        default : 'pending'
     },
     payment: {
         type: String,
-        default: 'inactive'
+        default : 'inactive'
     },
     method: {
         type: String,
-        required: true
+        required : true
     },
     image: {
         type: String,
@@ -36,8 +36,19 @@ const sellerSchema = new Schema({
     },
     shopInfo: {
         type: Object,
-        default: {}
+        default : {}
     },
-}, {timestamps : true});
+},{ timestamps: true })
 
-module.exports = model('sellers', sellerSchema);
+sellerSchema.index({
+    name: 'text',
+    email: 'text', 
+},{
+    weights: {
+        name: 5,
+        email: 4, 
+    }
+
+})
+
+module.exports = model('sellers',sellerSchema)
