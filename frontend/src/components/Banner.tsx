1dea5aa350d11d8f2eedd23a8f1eb6645926beec
Sers Théophile <theophile.sers@yahoo.com>
2024-06-22 13:16:20 +0200

    Update Banner.tsx with banner

--- frontend/src/components/Banner.tsx
+++ frontend/src/components/Banner.tsx
@@ -1,11 +1,16 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Carousel from 'react-multi-carousel';
 import { Link } from 'react-router-dom';
 import 'react-multi-carousel/lib/styles.css'
+import { useDispatch, useSelector } from 'react-redux';
+import { get_banners } from '../store/reducers/homeReducer';
 
 import '../scss/Banner.scss'
 
 const Banner = () => {
+
+    const dispatch = useDispatch()
+    const {banners} = useSelector(state => state.home)
 
     const responsive = {
 
@@ -28,6 +33,11 @@ const Banner = () => {
             items: 1
         },
     }
+
+    useEffect(() => {
+        dispatch(get_banners())
+    },[])
+
     return (
         <div className='Banner-container'>
             <div className='Banner-content'>
@@ -40,8 +50,8 @@ const Banner = () => {
                         showDots={true}
                         responsive={responsive}>
                         {
-                        [1,2,3,4,5,6].map((img, i) => <Link key={i} to='#'>
-                            <img src={`http://localhost:3000/public/images/banner/${img}.jpg`} alt="" />
+                        banners.length > 0 && banners.map((b, i) => <Link key={i} to={`product/details/${b.link}`}>
+                            <img src={ b.banner} alt="" />
                         </Link> )
                         }
                     </Carousel>        
