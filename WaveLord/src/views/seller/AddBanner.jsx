709e575c2f8d00e5ddaf2dc6dc4e6d8d515a98cc
Sers Théophile <theophile.sers@yahoo.com>
2024-06-22 12:08:58 +0200

    Update AddBanner.jsx dynamic image

--- WaveLord/src/views/seller/AddBanner.jsx
+++ WaveLord/src/views/seller/AddBanner.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FaRegImage } from "react-icons/fa";
 import { PropagateLoader } from 'react-spinners';
 import { overrideStyle } from '../../utils/utils';
@@ -6,20 +6,43 @@ import { overrideStyle } from '../../utils/utils';
 const AddBanner = () => {
   
     const loader = false
+    const [imageShow, setImageShow] = useState('')
+      const [image, setImage] = useState('')
+  
+      const imageHandle = (e) => {
+          const files = e.target.files 
+          const length = files.length
+  
+          if (length > 0) {
+              setImage(files[0])
+              setImageShow(URL.createObjectURL(files[0]))
+          } 
+      }
+  
+      const add = (e) => {
+          e.preventDefault()
+      }
+  
+
   
     return (
     <div>
         <h1>Add Banner</h1> 
         <div>
 
-          <form>
+          <form onSubmit={add}>
            <div >
               <label htmlFor="image">
                   <span><FaRegImage /></span>
                   <span>Select Banner Image </span>
               </label>
-              <input type="file" id='image' />
+              <input required onChange={imageHandle} type="file" id='image' />
               </div>
+                {
+                    imageShow && <div>
+                        <img src={imageShow} alt="" />
+                    </div>
+                }
   
               <button disabled={loader ? true : false}>
               {
