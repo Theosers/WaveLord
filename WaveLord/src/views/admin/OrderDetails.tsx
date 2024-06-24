b8fa65fdde52766b79e3d03422f334de10773f40
Sers Théophile <theophile.sers@yahoo.com>
2024-06-24 13:06:04 +0200

    Update and rename OrderDetails.jsx to OrderDetails.tsx

--- WaveLord/src/views/admin/OrderDetails.tsx
+++ WaveLord/src/views/admin/OrderDetails.tsx
@@ -1,49 +1,53 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, ChangeEvent } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
-import { admin_order_status_update, get_admin_order,messageClear} from '../../store/Reducers/OrderReducer';
+import { admin_order_status_update, get_admin_order, messageClear } from '../../store/Reducers/OrderReducer';
 import toast from 'react-hot-toast';
 import '../../scss/admin/OrderDetails.scss';
+import { RootState, AppDispatch } from '../../store';
 
-const OrderDetails = () => {
-
-    const dispatch = useDispatch() 
-    const [status, setStatus] = useState('')
-    const { order,errorMessage,successMessage } = useSelector(state => state.order)
-
-    useEffect(() => {
-        setStatus(order?.delivery_status)
-    },[order])
+const OrderDetails: React.FC = () => {
+    const dispatch = useDispatch<AppDispatch>();
+    const [status, setStatus] = useState<string>('');
+    const { order, errorMessage, successMessage } = useSelector((state: RootState) => state.order);
+    const { orderId } = useParams<{ orderId: string }>();
 
     useEffect(() => {
-        dispatch(get_admin_order(orderId))
-    },[orderId])
+        if (order) {
+            setStatus(order.delivery_status);
+        }
+    }, [order]);
 
-    const { orderId } = useParams() 
+    useEffect(() => {
+        if (orderId) {
+            dispatch(get_admin_order(orderId));
+        }
+    }, [orderId, dispatch]);
 
-    const status_update = (e) => {
-        dispatch(admin_order_status_update({orderId, info: {status: e.target.value} }))
-        setStatus(e.target.value)
-    }
-    
-    useEffect(() => { 
+    const status_update = (e: ChangeEvent<HTMLSelectElement>) => {
+        if (orderId) {
+            dispatch(admin_order_status_update({ orderId, info: { status: e.target.value } }));
+            setStatus(e.target.value);
+        }
+    };
+
+    useEffect(() => {
         if (successMessage) {
-            toast.success(successMessage)
-            dispatch(messageClear())  
-        } 
+            toast.success(successMessage);
+            dispatch(messageClear());
+        }
         if (errorMessage) {
-            toast.error(errorMessage)
-            dispatch(messageClear())  
-        } 
-    },[successMessage,errorMessage])
+            toast.error(errorMessage);
+            dispatch(messageClear());
+        }
+    }, [successMessage, errorMessage, dispatch]);
 
-    
     return (
         <div className='order-details-container'>
             <div className='order-details-box'>
                 <div className='title-container'>
                     <h2>Order Details</h2>
-                    <select onChange={status_update} value={status} name="" id="">
+                    <select onChange={status_update} value={status}>
                         <option value="pending">pending</option>
                         <option value="processing">processing</option>
                         <option value="warehouse">warehouse</option>
@@ -68,7 +72,6 @@ const OrderDetails = () => {
                             </div>
                             <span className='order-details-price'>Price : $920</span>
                         </div>
-
                         <div className='order-items'>
                             <img className='user-image' src='http://localhost:3000/src/assets/admin.jpeg' alt='' />
                             <div>
@@ -79,7 +82,6 @@ const OrderDetails = () => {
                                     <span>Quantity : 3</span>
                                 </p>
                             </div>
-
                         </div>
                         <div className='order-items'>
                             <img className='user-image' src='http://localhost:3000/src/assets/admin.jpeg' alt='' />
@@ -91,7 +93,6 @@ const OrderDetails = () => {
                                     <span>Quantity : 3</span>
                                 </p>
                             </div>
-
                         </div>
                         <div className='order-items'>
                             <img className='user-image' src='http://localhost:3000/src/assets/admin.jpeg' alt='' />
@@ -103,34 +104,29 @@ const OrderDetails = () => {
                                     <span>Quantity : 3</span>
                                 </p>
                             </div>
-
                         </div>
-                    
                     </div>
-                    
-                        
-                            {
-                            order?.suborder?.map((o,i) =>  <div className='right-box'>
-                                <div key={i + 20} className='seller-order'>
+                    {order?.suborder?.map((o, i) => (
+                        <div key={i} className='right-box'>
+                            <div className='seller-order'>
                                 <h2>Seller {i + 1} Order : </h2>
                                 <span>{o.delivery_status}</span>
-                                </div>
-                                {
-                                o.products?.map((p,i) => <div className='order-items'>
-                        
+                            </div>
+                            {o.products?.map((p, j) => (
+                                <div key={j} className='order-items'>
                                     <img className='user-image' src={p.images[0]} alt='' />
                                     <div>
-                                        <h2>{p.name} </h2>
+                                        <h2>{p.name}</h2>
                                         <p>
                                             <span>Brand : </span>
                                             <span>{p.brand}</span>
-                                            <span>Quantity : {p.quantity} </span>
+                                            <span>Quantity : {p.quantity}</span>
                                         </p>
                                     </div>
-                                </div>)
-                                }
-                            </div>
-                            )}
+                                </div>
+                            ))}
+                        </div>
+                    ))}
                 </div>
             </div>
         </div>
