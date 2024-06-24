b67cb256368ac709546ea48ded27206fd8587381
Sers Théophile <theophile.sers@yahoo.com>
2024-06-24 11:23:30 +0200

    Update and rename dashboardReducer.js to dashboardReducer.ts

---
+++ WaveLord/src/store/Reducers/dashboardReducer.ts
@@ -0,0 +1,79 @@
+import { createAsyncThunk, createSlice, PayloadAction } from "@reduxjs/toolkit";
+import api from "../../api/api";
+
+interface DashboardState {
+  totalSale: number;
+  totalOrder: number;
+  totalProduct: number;
+  totalPendingOrder: number;
+  totalSeller: number;
+  recentOrder: any[];
+  recentMessage: any[];
+  errorMessage?: string;
+}
+
+const initialState: DashboardState = {
+  totalSale: 0,
+  totalOrder: 0,
+  totalProduct: 0,
+  totalPendingOrder: 0,
+  totalSeller: 0,
+  recentOrder: [],
+  recentMessage: []
+};
+
+export const get_admin_dashboard_data = createAsyncThunk(
+  'dashboard/get_admin_dashboard_data',
+  async (_, { rejectWithValue, fulfillWithValue }) => {
+    try {
+      const { data } = await api.get('/admin/get-dashboard-data', { withCredentials: true });
+      return fulfillWithValue(data);
+    } catch (error) {
+      return rejectWithValue(error.response.data);
+    }
+  }
+);
+
+export const get_seller_dashboard_data = createAsyncThunk(
+  'dashboard/get_seller_dashboard_data',
+  async (_, { rejectWithValue, fulfillWithValue }) => {
+    try {
+      const { data } = await api.get('/seller/get-dashboard-data', { withCredentials: true });
+      return fulfillWithValue(data);
+    } catch (error) {
+      return rejectWithValue(error.response.data);
+    }
+  }
+);
+
+export const dashboardReducer = createSlice({
+  name: 'dashboard',
+  initialState,
+  reducers: {
+    messageClear: (state) => {
+      state.errorMessage = "";
+    }
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(get_admin_dashboard_data.fulfilled, (state, { payload }) => {
+        state.totalSale = payload.totalSale;
+        state.totalOrder = payload.totalOrder;
+        state.totalProduct = payload.totalProduct;
+        state.totalSeller = payload.totalSeller;
+        state.recentOrder = payload.recentOrders;
+        state.recentMessage = payload.messages;
+      })
+      .addCase(get_seller_dashboard_data.fulfilled, (state, { payload }) => {
+        state.totalSale = payload.totalSale;
+        state.totalOrder = payload.totalOrder;
+        state.totalProduct = payload.totalProduct;
+        state.totalPendingOrder = payload.totalPendingOrder;
+        state.recentOrder = payload.recentOrders;
+        state.recentMessage = payload.messages;
+      });
+  }
+});
+
+export const { messageClear } = dashboardReducer.actions;
+export default dashboardReducer.reducer;
