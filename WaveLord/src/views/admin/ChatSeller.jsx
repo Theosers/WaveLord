5dfabb587754154c1706f6fb71359caadc6801a6
Sers Théophile <theophile.sers@yahoo.com>
2024-06-24 11:59:00 +0200

    Update ChatSeller.tsx

--- WaveLord/src/views/admin/ChatSeller.jsx
+++ WaveLord/src/views/admin/ChatSeller.jsx
@@ -2,24 +2,35 @@ import React, { useEffect, useRef, useState } from 'react';
 import { FaList } from 'react-icons/fa6';
 import { IoMdClose } from "react-icons/io";
 import { useDispatch, useSelector } from 'react-redux';
-
 import { Link, useParams } from 'react-router-dom';
-import { FaRegFaceGrinHearts } from "react-icons/fa6";
 import toast from 'react-hot-toast';
-
 import '../../scss/admin/ChatSeller.scss';
-import { TRUE } from 'sass';
+import { RootState, AppDispatch } from '../../store';
 
-const ChatSeller = () => {
-    const scrollRef = useRef();
+interface Seller {
+    _id: string;
+    name: string;
+}
+
+const ChatSeller: React.FC = () => {
+    const scrollRef = useRef<HTMLDivElement>(null);
     const [show, setShow] = useState(false);
-    const { sellerId } = useParams();
+    const { sellerId } = useParams<{ sellerId: string }>();
     const [text, setText] = useState('');
     const [receverMessage, setReceverMessage] = useState('');
 
-    
-    const dispatch = useDispatch();
+    const dispatch = useDispatch<AppDispatch>();
 
+    const sellers: Seller[] = [
+        { _id: '0', name: 'gogogadgetoChapeau' },
+        { _id: '1', name: 'gogo' },
+        { _id: '2', name: 'gogo' },
+        { _id: '3', name: 'gogo' },
+        { _id: '4', name: 'gogo' },
+        { _id: '5', name: 'gogo' }
+    ];
+
+    const activeSellers = [0, 1, 2];
 
     return (
         <div className='chat-seller-container'>
@@ -28,72 +39,56 @@ const ChatSeller = () => {
                     <h2>Sellers</h2>
                     <IoMdClose className='chat-seller-close-icon' onClick={() => setShow(!show)} />
                 </div>
-                
-                {[{'_id':'0', 'name':'gogogadgetoChapeau'},{'_id':'0', 'name':'gogo'},{'_id':'0', 'name':'gogo'},
-                    {'_id':'0', 'name':'gogo'},{'_id':'0', 'name':'gogo'},{'_id':'0', 'name':'gogo'}
-                ].map((s, i) => (
+
+                {sellers.map((s, i) => (
                     <Link key={i} to={`/admin/dashboard/chat-sellers/${s._id}`} className={`chat-seller-subcontainer ${sellerId === s._id ? 'chat-seller-active' : ''}`}>
                         <div className='chat-seller-subcontainer'>
                             <img className='chat-seller-img' src="http://localhost:3000/src/assets/admin.jpeg" alt="" />
-                            {[0,1,2].some(a => a.sellerId === s._id) && <div className='chat-seller-active-indicator'></div>}
+                            {activeSellers.some(a => a === parseInt(s._id)) && <div className='chat-seller-active-indicator'></div>}
                             <h2 className='chat-seller-name'>{s.name}</h2>
                         </div>
-                        
-                       
-                        
-                        
                     </Link>
                 ))}
             </div>
             <div className={`chat-current-seller ${show ? 'chat-current-seller-off' : ''}`}>
-
-                <div className='chat-current-seller-title-container' >
+                <div className='chat-current-seller-title-container'>
                     <img className='chat-seller-img' src="http://localhost:3000/src/assets/admin.jpeg" alt="" />
-
                     <FaList className='chat-seller-list-icon' onClick={() => setShow(!show)} />
                 </div>
-                
-                
+
                 <div className='chat-box'>
-                <div className='left-person'>
-                    <img className='chat-seller-img' src="http://localhost:3000/src/assets/admin.jpeg" alt="" />
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse, dolores eum! Hic minus, ex commodi, a incidunt obcaecati culpa sed quaerat nostrum accusantium, sint tempore autem officiis possimus repudiandae fugit?</p>
-                </div>
-                <div className='right-person'>
-                    <img className='chat-seller-img' src="http://localhost:3000/src/assets/admin.jpeg" alt="" />
-                    <p>How are you ?</p>
-                </div>
-                <div className='left-person'>
-                    <img className='chat-seller-img' src="http://localhost:3000/src/assets/admin.jpeg" alt="" />
-                    <p>How are you ?</p>
-                </div>
-                <div className='right-person'>
-                    <img className='chat-seller-img' src="http://localhost:3000/src/assets/admin.jpeg" alt="" />
-                    <p>How are you ?</p>
-                </div>
-
-                <div className='left-person'>
-                    <img className='chat-seller-img' src="http://localhost:3000/src/assets/admin.jpeg" alt="" />
-                    <p>How are you ?</p>
-                </div>
-                <div className='right-person'>
-                    <img className='chat-seller-img' src="http://localhost:3000/src/assets/admin.jpeg" alt="" />
-                    <p>How are you ?</p>
-                </div>
-                <div className='left-person'>
-                    <img className='chat-seller-img' src="http://localhost:3000/src/assets/admin.jpeg" alt="" />
-                    <p>How are you ?</p>
-                </div>
-                <div className='right-person'>
-                    <img className='chat-seller-img' src="http://localhost:3000/src/assets/admin.jpeg" alt="" />
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi facilis mollitia recusandae eos est repudiandae voluptate ullam, quasi ipsam vero omnis reprehenderit aliquam. Ullam tempora consequatur accusamus quasi, omnis eum.</p>
-                </div>
-
-
-
-
-
-
+                    <div className='left-person'>
+                        <img className='chat-seller-img' src="http://localhost:3000/src/assets/admin.jpeg" alt="" />
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse, dolores eum! Hic minus, ex commodi, a incidunt obcaecati culpa sed quaerat nostrum accusantium, sint tempore autem officiis possimus repudiandae fugit?</p>
+                    </div>
+                    <div className='right-person'>
+                        <img className='chat-seller-img' src="http://localhost:3000/src/assets/admin.jpeg" alt="" />
+                        <p>How are you ?</p>
+                    </div>
+                    <div className='left-person'>
+                        <img className='chat-seller-img' src="http://localhost:3000/src/assets/admin.jpeg" alt="" />
+                        <p>How are you ?</p>
+                    </div>
+                    <div className='right-person'>
+                        <img className='chat-seller-img' src="http://localhost:3000/src/assets/admin.jpeg" alt="" />
+                        <p>How are you ?</p>
+                    </div>
+                    <div className='left-person'>
+                        <img className='chat-seller-img' src="http://localhost:3000/src/assets/admin.jpeg" alt="" />
+                        <p>How are you ?</p>
+                    </div>
+                    <div className='right-person'>
+                        <img className='chat-seller-img' src="http://localhost:3000/src/assets/admin.jpeg" alt="" />
+                        <p>How are you ?</p>
+                    </div>
+                    <div className='left-person'>
+                        <img className='chat-seller-img' src="http://localhost:3000/src/assets/admin.jpeg" alt="" />
+                        <p>How are you ?</p>
+                    </div>
+                    <div className='right-person'>
+                        <img className='chat-seller-img' src="http://localhost:3000/src/assets/admin.jpeg" alt="" />
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi facilis mollitia recusandae eos est repudiandae voluptate ullam, quasi ipsam vero omnis reprehenderit aliquam. Ullam tempora consequatur accusamus quasi, omnis eum.</p>
+                    </div>
                 </div>
                 <form className='chat-form' action="">
                     <input type="text" />
